7a3c9f56e7d33901d675899b55c72932cbec9a30
Your Name <you@example.com>
2025-10-13 20:27:58 -0300

    Meu segundo commit

---
+++ Home.js
@@ -0,0 +1,1 @@
+alert("Eus sou a Home")
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 alert("Curso de Mobile Semana 01 - Santander");
 
 alert("Curso de Mobile Semana 02 - Santander");
+
+alert("Curso de Mobile Semana 03 - Santander");
